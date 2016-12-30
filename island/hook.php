3c2d82bb1adcf9118dee101707dfd03801781b42
Jacob <jacob.ralph@diamondprofilellc.com>
2016-12-30 13:13:53 -0500

    Fix.

--- island/hook.php
+++ island/hook.php
@@ -1,6 +1,6 @@
 <?php
 // Use in the "Post-Receive URLs" section of your GitHub repo.
 if ( $_POST['payload'] ) {
-  shell_exec( 'cd /var/www/html/staging/vrapp/island/ && git reset --hard HEAD && git pull' );
+  shell_exec( 'cd /var/www/html/staging/vrapp/github/ && git reset --hard HEAD && git pull' );
 }
 ?>
