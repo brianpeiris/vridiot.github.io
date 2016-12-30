ac3ff20941009e9c62354e72266db6ebae2b6679
Jacob <jacob.ralph@diamondprofilellc.com>
2016-12-30 14:28:45 -0500

    sync

--- island/hook.php
+++ island/hook.php
@@ -1,4 +1,8 @@
 <?php
 // Use in the "Post-Receive URLs" section of your GitHub repo.
   echo shell_exec( 'cd /var/www/html/staging/vrapp/github/vridiot.github.io/ && sudo git reset --hard HEAD && sudo git pull' );
+  $myfile = fopen("lastupdate.txt", "w") or die("Unable to open file!");
+  $txt = time ();
+  fwrite($myfile, $txt);
+  fclose($myfile);
 ?>
