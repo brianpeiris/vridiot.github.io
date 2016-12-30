81d2bd921cdd97b996c56bd2a1aac84407c9922b
Jacob <jacob.ralph@diamondprofilellc.com>
2016-12-30 13:57:15 -0500

    Fix.

--- island/hook.php
+++ island/hook.php
@@ -1,4 +1,4 @@
 <?php
 // Use in the "Post-Receive URLs" section of your GitHub repo.
-  shell_exec( 'cd /var/www/html/staging/vrapp/github/vridiot.github.io/ && sudo git reset --hard HEAD && sudo git pull' );
+  exec( 'cd /var/www/html/staging/vrapp/github/vridiot.github.io/ && sudo git reset --hard HEAD && sudo git pull' );
 ?>
