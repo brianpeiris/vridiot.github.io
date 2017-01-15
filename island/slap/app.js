65ebd638a6934ff758f2b28df271b617fa713970
Jacob <jacob.ralph@diamondprofilellc.com>
2017-01-14 19:29:30 -0500

    Fixes.

--- island/slap/app.js
+++ island/slap/app.js
@@ -4,6 +4,7 @@ var position_x = 0;
 var position_y = 0;
 var position_z = 0;
 io.on('connection', function(socket) {  
+	console.log("New Connection");
     socket.emit('position', { pos: {x: position_x, y: position_y, z: position_z} });
 
     socket.on('update_position', function(data) {
@@ -11,6 +12,8 @@ io.on('connection', function(socket) {
     		position_x = data.position.x;
     		position_y = data.position.y;
     		position_z = data.position.z;
+    		console.log("Receiving Coordinate");
+    		console.log(data.position);
     		io.emit('position', { pos: {x: position_x, y: position_y, z: position_z} });
     	}
     });
