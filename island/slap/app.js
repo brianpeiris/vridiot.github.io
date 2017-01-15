83660cdbd24dc40e0e2746cfc2cc4196bfa1d25e
Jacob <jacob.ralph@diamondprofilellc.com>
2017-01-14 19:50:11 -0500

    Changes.

--- island/slap/app.js
+++ island/slap/app.js
@@ -4,7 +4,6 @@ var position_y = 0;
 var position_z = 0;
 console.log("Running!");
 io.on('connection', function(socket) {  
-	console.log("New Connection");
     socket.emit('position', { pos: {x: position_x, y: position_y, z: position_z} });
 
     socket.on('update_position', function(data) {
@@ -12,8 +11,6 @@ io.on('connection', function(socket) {
     		position_x = data.position.x;
     		position_y = data.position.y;
     		position_z = data.position.z;
-    		console.log("Receiving Coordinate");
-    		console.log(data.position);
     		io.emit('position', { pos: {x: position_x, y: position_y, z: position_z} });
     	}
     });
