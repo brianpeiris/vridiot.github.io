614e94e5e34c3ede820f8c379238547db52da0e2
Jacob <jacob.ralph@diamondprofilellc.com>
2017-01-14 20:06:42 -0500

    Ffffixes.

--- island/slap/app.js
+++ island/slap/app.js
@@ -5,14 +5,16 @@ var position_z = 0;
 console.log("Running!");
 io.on('connection', function(socket) {  
 console.log("New Connection!");
-    socket.emit('position', { pos: {x: position_x, y: position_y, z: position_z} });
+    var send = {x: position_x, y: position_y, z: position_z};
+    socket.emit('position', send);
 
     socket.on('update_position', function(data) {
     	if(data && data.position){
     		position_x = data.position.x;
     		position_y = data.position.y;
     		position_z = data.position.z;
-    		io.emit('position', { pos: {x: position_x, y: position_y, z: position_z} });
+    		var send = {x: position_x, y: position_y, z: position_z};
+    		io.emit('position', send);
     	}
     });
 
